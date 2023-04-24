942c63b5278ad4a0761f40b382a7fb7515892fd8
shin-mallang <huipulci1@naver.com>
2023-04-24 20:40:04 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/chat/woowa/woowachat/WoowachatApplication.java
@@ -0,0 +1,13 @@
+package chat.woowa.woowachat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WoowachatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WoowachatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/chat/woowa/woowachat/WoowachatApplicationTests.java
@@ -0,0 +1,13 @@
+package chat.woowa.woowachat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WoowachatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
