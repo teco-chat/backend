039b4f74cc7bec9ea9639f7eb33c713fead3575c
greeng00se <thegreengoose7@gmail.com>
2023-04-25 17:44:46 +0900

    [#4] feat: Chat 메시지 받환하는 기능 구현

--- src/test/java/chat/woowa/woowachat/chat/ChatTest.java
+++ src/test/java/chat/woowa/woowachat/chat/ChatTest.java
@@ -1,12 +1,14 @@
 package chat.woowa.woowachat.chat;
 
+import static chat.woowa.woowachat.chat.SettingMessage.BACK_END_SETTING;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import chat.woowa.woowachat.chat.fixture.ChatFixture;
+import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)
@@ -25,5 +27,24 @@ class ChatTest {
         assertThat(chat.messages())
                 .extracting(Message::content)
                 .containsExactly("안녕", "응 안녕");
+    }
+
+    @Test
+    void 입력받은_토큰보다_메시지의_토큰_총합이_낮도록_오래된_순으로_메시지를_제외한_후_반환한다() {
+        // given
+        int token = 2000;
+        final List<Message> messages = List.of(
+                Message.user("Hello1", 1500),
+                Message.user("Hello2", 200),
+                Message.user("Hello3", 1500)
+        );
+        final Chat chat = ChatFixture.chatWithMessages("안녕", messages);
+
+        // when
+        final List<Message> result = chat.lessThan(token);
+
+        // then
+        assertThat(result).extracting(Message::content)
+                .containsExactly(BACK_END_SETTING.message(), "Hello2", "Hello3");
     }
 }
--- src/test/java/chat/woowa/woowachat/chat/fixture/ChatFixture.java
+++ src/test/java/chat/woowa/woowachat/chat/fixture/ChatFixture.java
@@ -1,10 +1,11 @@
 package chat.woowa.woowachat.chat.fixture;
 
-import chat.woowa.woowachat.chat.Chat;
-import chat.woowa.woowachat.chat.Message;
-
 import static chat.woowa.woowachat.chat.GptModel.GPT_3_5_TURBO;
 import static chat.woowa.woowachat.chat.SettingMessage.BACK_END_SETTING;
+
+import chat.woowa.woowachat.chat.Chat;
+import chat.woowa.woowachat.chat.Message;
+import java.util.List;
 
 public class ChatFixture {
 
@@ -17,6 +18,19 @@ public class ChatFixture {
 
         for (final String message : messages) {
             chat.addMessage(Message.user(message, 1));
+        }
+        return chat;
+    }
+
+    public static Chat chatWithMessages(final String first, final List<Message> messages) {
+        final Chat chat = new Chat(GPT_3_5_TURBO,
+                BACK_END_SETTING,
+                first,
+                1L,
+                messages.get(0));
+
+        for (Message message : messages.stream().skip(1).toList()) {
+            chat.addMessage(message);
         }
         return chat;
     }
