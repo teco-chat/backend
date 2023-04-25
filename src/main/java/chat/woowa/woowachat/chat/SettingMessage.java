89153b5df065ba9f4fe37f4e84ed7ccbe6fb0043
shin-mallang <huipulci1@naver.com>
2023-04-25 09:25:48 +0900

    [#1] feat: 기본 언어 한국어로 설정

--- src/main/java/chat/woowa/woowachat/chat/SettingMessage.java
+++ src/main/java/chat/woowa/woowachat/chat/SettingMessage.java
@@ -2,15 +2,17 @@ package chat.woowa.woowachat.chat;
 
 public enum SettingMessage {
 
-    BACK_END_SETTING("You are a helpful backend developer assistant"),
-    FRONT_END_SETTING("You are a helpful frontend developer assistant"),
-    ANDROID_SETTING("You are a helpful android developer assistant"),
+    BACK_END_SETTING("You are a helpful backend developer assistant."),
+    FRONT_END_SETTING("You are a helpful frontend developer assistant."),
+    ANDROID_SETTING("You are a helpful android developer assistant."),
     ;
+
+    private static final String DEFAULT_LANGUAGE_SETTING = " If there is no request, please reply in Korean.";
 
     private final String message;
 
     SettingMessage(final String message) {
-        this.message = message;
+        this.message = message + DEFAULT_LANGUAGE_SETTING;
     }
 
     public String message() {
