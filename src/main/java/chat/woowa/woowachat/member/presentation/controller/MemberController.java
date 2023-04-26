8b91afa9ee60668f61363b4faf6466fcdb1e93a8
shin-mallang <huipulci1@naver.com>
2023-04-26 09:45:38 +0900

    [#3] feat: 회원가입 api 구현

---
+++ src/main/java/chat/woowa/woowachat/member/presentation/controller/MemberController.java
@@ -0,0 +1,26 @@
+package chat.woowa.woowachat.member.presentation.controller;
+
+import static org.springframework.http.HttpStatus.CREATED;
+
+import chat.woowa.woowachat.member.dto.SignUpDto;
+import chat.woowa.woowachat.member.service.MemberService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MemberController {
+
+    private final MemberService memberService;
+
+    public MemberController(final MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @PostMapping("/members")
+    ResponseEntity<Void> signUp(@RequestBody final SignUpDto signUpDto) {
+        memberService.signUp(signUpDto);
+        return ResponseEntity.status(CREATED.value()).build();
+    }
+}
