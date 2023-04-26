74cecd58534900e34c00d2267549edc1d94a690c
shin-mallang <huipulci1@naver.com>
2023-04-26 23:27:48 +0900

    [#5] refactor: SettingMessage 토큰 수 줄이기, 참고 주석 추가

--- src/main/java/chat/woowa/woowachat/chat/domain/SettingMessage.java
+++ src/main/java/chat/woowa/woowachat/chat/domain/SettingMessage.java
@@ -2,12 +2,13 @@ package chat.woowa.woowachat.chat.domain;
 
 public enum SettingMessage {
 
+    // (참고) 토큰 수는 31 나옴
     BACK_END_SETTING("You are a helpful backend developer assistant."),
     FRONT_END_SETTING("You are a helpful frontend developer assistant."),
     ANDROID_SETTING("You are a helpful android developer assistant."),
     ;
 
-    private static final String DEFAULT_LANGUAGE_SETTING = " If there is no request, please reply in Korean.";
+    private static final String DEFAULT_LANGUAGE_SETTING = "If there is no request, please reply in Korean.";
 
     private final String message;
 
