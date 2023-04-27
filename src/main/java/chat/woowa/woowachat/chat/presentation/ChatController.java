8055996c07acaae16da92a3f334486a7adbfb46e
shin-mallang <huipulci1@naver.com>
2023-04-27 15:53:15 +0900

    [#11] feat: 채팅 검색 컨트롤러 구현

--- src/main/java/chat/woowa/woowachat/chat/presentation/ChatController.java
+++ src/main/java/chat/woowa/woowachat/chat/presentation/ChatController.java
@@ -4,13 +4,19 @@ import static org.springframework.http.HttpStatus.CREATED;
 
 import chat.woowa.woowachat.chat.application.AskChatService;
 import chat.woowa.woowachat.chat.application.ChatQueryService;
+import chat.woowa.woowachat.chat.domain.ChatQueryRepository.ChatSearchCond;
 import chat.woowa.woowachat.chat.dto.AskCommand;
 import chat.woowa.woowachat.chat.dto.AskRequest;
 import chat.woowa.woowachat.chat.dto.ChatQueryDto;
+import chat.woowa.woowachat.chat.dto.ChatSearchQueryDto;
 import chat.woowa.woowachat.chat.dto.MessageDto;
+import chat.woowa.woowachat.common.presentation.PageResponse;
 import chat.woowa.woowachat.member.presentation.argumentresolver.Auth;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -56,5 +62,14 @@ public class ChatController {
             @PathVariable final Long id
     ) {
         return ResponseEntity.ok(chatQueryService.findById(id));
+    }
+
+    @GetMapping("/chats")
+    public ResponseEntity<PageResponse<ChatSearchQueryDto>> search(
+            @ModelAttribute final ChatSearchCond cond,
+            @PageableDefault(size = 20) final Pageable pageable
+    ) {
+        return ResponseEntity.ok(
+                PageResponse.from(chatQueryService.search(cond, pageable)));
     }
 }
