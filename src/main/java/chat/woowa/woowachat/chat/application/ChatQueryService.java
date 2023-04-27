79c5f192f9d8d91fd1bbe8adf955938f9092f5fb
shin-mallang <huipulci1@naver.com>
2023-04-27 12:47:43 +0900

    [#11] feat: 채팅 검색 서비스 구현

--- src/main/java/chat/woowa/woowachat/chat/application/ChatQueryService.java
+++ src/main/java/chat/woowa/woowachat/chat/application/ChatQueryService.java
@@ -1,10 +1,15 @@
 package chat.woowa.woowachat.chat.application;
 
 import chat.woowa.woowachat.chat.domain.Chat;
+import chat.woowa.woowachat.chat.domain.ChatQueryRepository;
+import chat.woowa.woowachat.chat.domain.ChatQueryRepository.ChatSearchCond;
 import chat.woowa.woowachat.chat.domain.ChatRepository;
 import chat.woowa.woowachat.chat.dto.ChatQueryDto;
+import chat.woowa.woowachat.chat.dto.ChatSearchQueryDto;
 import chat.woowa.woowachat.member.domain.Member;
 import chat.woowa.woowachat.member.domain.MemberRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,17 +19,30 @@ public class ChatQueryService {
 
     private final MemberRepository memberRepository;
     private final ChatRepository chatRepository;
+    private final ChatQueryRepository chatQueryRepository;
 
-    public ChatQueryService(final MemberRepository memberRepository, final ChatRepository chatRepository) {
+    public ChatQueryService(final MemberRepository memberRepository,
+                            final ChatRepository chatRepository,
+                            final ChatQueryRepository chatQueryRepository) {
         this.memberRepository = memberRepository;
         this.chatRepository = chatRepository;
+        this.chatQueryRepository = chatQueryRepository;
     }
 
     public ChatQueryDto findById(final Long id) {
         final Chat chat = chatRepository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("아이디가 %d인 채팅이 없습니다."));
-        final Member member = memberRepository.findById(chat.memberId())
-                .orElseThrow(() -> new IllegalArgumentException("아이디가 %d인 회원 없습니다."));
+        final Member member = findMemberById(chat.memberId());
         return ChatQueryDto.of(chat, member.name(), member.course());
+    }
+
+    public Page<ChatSearchQueryDto> search(final ChatSearchCond cond, final Pageable pageable) {
+        return chatQueryRepository.search(cond, pageable)
+                .map(it -> ChatSearchQueryDto.from(it, findMemberById(it.memberId())));
+    }
+
+    private Member findMemberById(final Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("아이디가 %d인 회원 없습니다."));
     }
 }
---
+++ src/main/java/chat/woowa/woowachat/chat/dto/ChatSearchQueryDto.java
@@ -0,0 +1,26 @@
+package chat.woowa.woowachat.chat.dto;
+
+import chat.woowa.woowachat.chat.domain.Chat;
+import chat.woowa.woowachat.member.domain.Course;
+import chat.woowa.woowachat.member.domain.Member;
+import java.time.LocalDateTime;
+
+public record ChatSearchQueryDto(
+        Long id,
+        Long crewId,
+        String crewName,
+        Course course,
+        String title,
+        LocalDateTime createdAt
+) {
+    public static ChatSearchQueryDto from(final Chat chat, final Member member) {
+        return new ChatSearchQueryDto(
+                chat.id(),
+                member.id(),
+                member.name(),
+                member.course(),
+                chat.title(),
+                chat.createdAt()
+        );
+    }
+}
