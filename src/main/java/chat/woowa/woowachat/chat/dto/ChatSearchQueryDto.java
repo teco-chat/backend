94221263b5a3bab991e9e39d67454fac91122301
Donghun Shin <huipulci1@naver.com>
2023-04-28 09:50:40 +0900

    Revert "[#11] feature 채팅 검색 기능 구현" (#16)
    * Revert "[#11] feature 채팅 검색 기능 구현"
    
    * [#11] chore: queryDsl 설정
    
    * [#11] feat: 채팅 검색 repository 구현
    
    * [#11] feat: 채팅 검색 서비스 구현
    
    * refactor: 테스트코드 잘못된 패키지 수정
    
    * [#11] test: 오작동하는 테스트코드 수장
    
    * [#11] feat: 채팅 검색 컨트롤러 구현
    
    * [#11] test: restAssured log 추가
    
    * [#11] style: 주석 순서 변경

--- src/main/java/chat/woowa/woowachat/chat/dto/ChatSearchQueryDto.java
+++ src/main/java/chat/woowa/woowachat/chat/dto/ChatSearchQueryDto.java
@@ -1,6 +1,8 @@
 package chat.woowa.woowachat.chat.dto;
 
+import chat.woowa.woowachat.chat.domain.Chat;
 import chat.woowa.woowachat.member.domain.Course;
+import chat.woowa.woowachat.member.domain.Member;
 import java.time.LocalDateTime;
 
 public record ChatSearchQueryDto(
@@ -11,4 +13,14 @@ public record ChatSearchQueryDto(
         String title,
         LocalDateTime createdAt
 ) {
+    public static ChatSearchQueryDto from(final Chat chat, final Member member) {
+        return new ChatSearchQueryDto(
+                chat.id(),
+                member.id(),
+                member.name(),
+                member.course(),
+                chat.title(),
+                chat.createdAt()
+        );
+    }
 }
