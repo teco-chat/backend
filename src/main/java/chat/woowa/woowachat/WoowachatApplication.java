74aad50e5f33200308b7259a469741b183209fbc
shin-mallang <huipulci1@naver.com>
2023-04-29 15:27:59 +0900

    [#22] feature: TimeZone을 서울 기준으로 설정

--- src/main/java/chat/woowa/woowachat/WoowachatApplication.java
+++ src/main/java/chat/woowa/woowachat/WoowachatApplication.java
@@ -1,5 +1,6 @@
 package chat.woowa.woowachat;
 
+import java.util.TimeZone;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class WoowachatApplication {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(WoowachatApplication.class, args);
     }
 }
