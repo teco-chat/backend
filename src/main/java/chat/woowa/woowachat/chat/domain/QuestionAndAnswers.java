91f4f6a98b4608e33337ac3c193a5c9749bfa482
shin-mallang <huipulci1@naver.com>
2023-04-29 19:07:07 +0900

    [#25] feature: 질문과 답변을 관리할 QnA 생성

---
+++ src/main/java/chat/woowa/woowachat/chat/domain/QuestionAndAnswers.java
@@ -0,0 +1,58 @@
+package chat.woowa.woowachat.chat.domain;
+
+import static jakarta.persistence.CascadeType.ALL;
+import static jakarta.persistence.FetchType.LAZY;
+
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.List;
+
+@Embeddable
+public class QuestionAndAnswers {
+
+    @OneToMany(fetch = LAZY, cascade = ALL, orphanRemoval = true)
+    @JoinColumn(name = "chat_id")
+    private List<QuestionAndAnswer> questionAndAnswers = new ArrayList<>();
+
+    protected QuestionAndAnswers() {
+    }
+
+    public QuestionAndAnswers(final QuestionAndAnswer... questionAndAnswers) {
+        this(Arrays.asList(questionAndAnswers));
+    }
+
+    public QuestionAndAnswers(final List<QuestionAndAnswer> questionAndAnswers) {
+        this.questionAndAnswers.addAll(questionAndAnswers);
+    }
+
+    public void add(final QuestionAndAnswer questionAndAnswer) {
+        questionAndAnswers.add(questionAndAnswer);
+    }
+
+    public List<QuestionAndAnswer> lessOrEqualThan(final int token) {
+        final Deque<QuestionAndAnswer> result = new ArrayDeque<>(this.questionAndAnswers);
+        int tokenSum = calculateTokenSum();
+
+        while (tokenSum > token) {
+            final QuestionAndAnswer message = result.removeFirst();
+            tokenSum -= message.token();
+        }
+
+        return new ArrayList<>(result);
+    }
+
+    private int calculateTokenSum() {
+        return questionAndAnswers.stream()
+                .mapToInt(QuestionAndAnswer::token)
+                .sum();
+    }
+
+    public List<QuestionAndAnswer> questionAndAnswers() {
+        return new ArrayList<>(questionAndAnswers);
+    }
+}
