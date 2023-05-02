75cad44c157a3cbfe264821da285288890cf4dbb
shin-mallang <huipulci1@naver.com>
2023-05-02 11:59:33 +0900

    [#36] refactor: GPT API 호출을 트랜잭션 범위에서 분리

--- src/main/java/chat/woowa/woowachat/chat/application/AskChatService.java
+++ src/main/java/chat/woowa/woowachat/chat/application/AskChatService.java
@@ -12,57 +12,61 @@ import chat.woowa.woowachat.chat.dto.MessageDto;
 import chat.woowa.woowachat.member.domain.Member;
 import chat.woowa.woowachat.member.domain.MemberRepository;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
-@Transactional
 @Service
 public class AskChatService {
 
     private final MemberRepository memberRepository;
     private final ChatRepository chatRepository;
     private final GptClient gptClient;
+    private final TransactionTemplate transactionTemplate;
 
-    public AskChatService(final MemberRepository memberRepository,
-                          final ChatRepository chatRepository,
-                          final GptClient gptClient) {
+    public AskChatService(final MemberRepository memberRepository, final ChatRepository chatRepository,
+                          final GptClient gptClient, final TransactionTemplate transactionTemplate) {
         this.memberRepository = memberRepository;
         this.chatRepository = chatRepository;
         this.gptClient = gptClient;
+        this.transactionTemplate = transactionTemplate;
     }
 
     public MessageDto createAsk(final AskCommand command) {
         final Question question = command.question();
-        final Chat chat = saveInitialChat(command, question);
-        return answer(chat, question);
-    }
-
-    private Chat saveInitialChat(final AskCommand command, final Question question) {
         final Member member = findMemberById(command.memberId());
         Chat chat = new Chat(GptModel.GPT_3_5_TURBO,
                 SettingMessage.byCourse(member.course()),
                 question.content(),
                 command.memberId()
         );
-        return chatRepository.save(chat);
+
+        final QuestionAndAnswer qna = gptClient.ask(chat, question);
+
+        return transactionTemplate.execute(status -> {
+            chatRepository.save(chat);
+            chat.addQuestionAndAnswer(qna);
+            return new MessageDto(chat.id(), qna.answer().content());
+        });
     }
 
     public MessageDto ask(final Long id, final AskCommand command) {
-        final Chat chat = chatRepository.findById(id)
-                .orElseThrow(() -> new IllegalArgumentException("아이디가 %d인 채팅이 없습니다."));
-        return answer(chat, command.question());
+        final Chat chat = findChatWithQuestionAndAnswersById(id);
+
+        final QuestionAndAnswer qna = gptClient.ask(chat, command.question());
+
+        return transactionTemplate.execute(status -> {
+            findChatWithQuestionAndAnswersById(id).addQuestionAndAnswer(qna);
+            return new MessageDto(chat.id(), qna.answer().content());
+        });
     }
 
     private Member findMemberById(final Long memberId) {
-        // TODO 예외처리 변경
         return memberRepository.findById(memberId)
                 .orElseThrow(() ->
-                        new IllegalArgumentException("일치하는 회원 ID가 없습니다. (TODO: 변경)")
-                );
+                        new IllegalArgumentException("일치하는 회원 ID가 없습니다. (TODO: 변경)"));
     }
 
-    private MessageDto answer(final Chat chat, final Question question) {
-        final QuestionAndAnswer ask = gptClient.ask(chat, question);
-        chat.addQuestionAndAnswer(ask);
-        return new MessageDto(chat.id(), ask.answer().content());
+    private Chat findChatWithQuestionAndAnswersById(final Long id) {
+        return chatRepository.findWithQuestionAndAnswersById(id)
+                .orElseThrow(() -> new IllegalArgumentException("아이디가 %d인 채팅이 없습니다."));
     }
 }
