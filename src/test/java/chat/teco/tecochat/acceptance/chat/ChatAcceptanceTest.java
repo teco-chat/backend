1d20aca32218935746f20bfc318408e7a0c4adfc
shin-mallang <huipulci1@naver.com>
2023-05-16 23:34:56 +0900

    [#61] fix: 테스트 실패 오류 수정 - DB 데이터가 지워지지 않아서 발생한 오류였음

--- src/test/java/chat/teco/tecochat/acceptance/chat/ChatAcceptanceTest.java
+++ src/test/java/chat/teco/tecochat/acceptance/chat/ChatAcceptanceTest.java
@@ -25,6 +25,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
