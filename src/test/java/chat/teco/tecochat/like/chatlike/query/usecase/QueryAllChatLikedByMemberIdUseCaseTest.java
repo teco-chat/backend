7a419805c00e6192ff1787aea8d0bc654f0b8490
greeng00se <thegreengoose7@gmail.com>
2023-05-19 09:40:57 +0900

    [#73] feat: 내가 좋아요 누른 게시물 형식 변경
    Co-authored-by: shin-mallang <huipulci1@naver.com>

--- src/test/java/chat/teco/tecochat/like/chatlike/query/usecase/QueryAllChatLikedByMemberIdUseCaseTest.java
+++ src/test/java/chat/teco/tecochat/like/chatlike/query/usecase/QueryAllChatLikedByMemberIdUseCaseTest.java
@@ -7,14 +7,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import chat.teco.tecochat.like.chatlike.query.ChatLikeQueryUseCaseTest;
 import chat.teco.tecochat.like.chatlike.query.usecase.QueryAllChatLikedByMemberIdUseCase.QueryChatLikeByMemberIdResponse;
-import chat.teco.tecochat.like.chatlike.query.usecase.QueryAllChatLikedByMemberIdUseCase.QueryChatLikeByMemberIdResponse.ChatInfo;
 import chat.teco.tecochat.member.domain.Course;
-import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.DisplayNameGeneration;
 import org.junit.jupiter.api.DisplayNameGenerator.ReplaceUnderscores;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 
 @SuppressWarnings("NonAsciiCharacters")
 @DisplayNameGeneration(ReplaceUnderscores.class)
@@ -27,12 +27,12 @@ class QueryAllChatLikedByMemberIdUseCaseTest extends ChatLikeQueryUseCaseTest {
     @Test
     void 회원이_누른_좋아요를_조회한다() {
         // when
-        List<QueryChatLikeByMemberIdResponse> allByMemberId =
-                queryAllChatLikedByMemberIdUseCase.findAllByMemberId(허브.id());
+        Page<QueryChatLikeByMemberIdResponse> allByMemberId =
+                queryAllChatLikedByMemberIdUseCase.findAllByMemberId(허브.id(), PageRequest.of(0, 20));
 
         // then
-        회원으로_조회한_좋아요의_정보를_검증한다(allByMemberId.get(0),
-                허브_박스터채팅_좋아요.id(),
+        회원으로_조회한_좋아요의_정보를_검증한다(
+                allByMemberId.getContent().get(0),
                 박스터_채팅.id(),
                 박스터_채팅.memberId(),
                 "박스터",
@@ -41,8 +41,8 @@ class QueryAllChatLikedByMemberIdUseCaseTest extends ChatLikeQueryUseCaseTest {
                 3,
                 0);
 
-        회원으로_조회한_좋아요의_정보를_검증한다(allByMemberId.get(1),
-                허브_허브채팅_좋아요.id(),
+        회원으로_조회한_좋아요의_정보를_검증한다(
+                allByMemberId.getContent().get(1),
                 허브_채팅.id(),
                 허브_채팅.memberId(),
                 "허브_좋아요",
@@ -51,8 +51,8 @@ class QueryAllChatLikedByMemberIdUseCaseTest extends ChatLikeQueryUseCaseTest {
                 3,
                 0);
 
-        회원으로_조회한_좋아요의_정보를_검증한다(allByMemberId.get(2),
-                허브_말랑채팅_좋아요.id(),
+        회원으로_조회한_좋아요의_정보를_검증한다(
+                allByMemberId.getContent().get(2),
                 말랑_채팅.id(),
                 말랑_채팅.memberId(),
                 "말랑_좋아요",
@@ -63,8 +63,7 @@ class QueryAllChatLikedByMemberIdUseCaseTest extends ChatLikeQueryUseCaseTest {
     }
 
     private void 회원으로_조회한_좋아요의_정보를_검증한다(
-            QueryChatLikeByMemberIdResponse memberIdQueryDto,
-            Long chatLikeId,
+            QueryChatLikeByMemberIdResponse chatInfo,
             Long chatId,
             Long crewId,
             String crewName,
@@ -73,8 +72,6 @@ class QueryAllChatLikedByMemberIdUseCaseTest extends ChatLikeQueryUseCaseTest {
             int likeCount,
             int totalQnaCount
     ) {
-        assertThat(memberIdQueryDto.id()).isEqualTo(chatLikeId);
-        ChatInfo chatInfo = memberIdQueryDto.chatInfo();
         assertThat(chatInfo.id()).isEqualTo(chatId);
         assertThat(chatInfo.crewId()).isEqualTo(crewId);
         assertThat(chatInfo.crewName()).isEqualTo(crewName);
