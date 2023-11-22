caa2a65978a722baf51a7a6ac800c7341547ccf9
greeng00se <thegreengoose7@gmail.com>
2023-11-22 22:01:09 +0900

    [#113] refactor: Cors 설정 수정

--- src/main/kotlin/chat/teco/tecochat/config/CorsConfig.kt
+++ src/main/kotlin/chat/teco/tecochat/config/CorsConfig.kt
@@ -1,10 +1,12 @@
 package chat.teco.tecochat.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.http.HttpMethod
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
+@Profile("!prod")
 @Configuration
 class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
